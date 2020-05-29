fb2f0256886864a513ba5774c4c337069193840e
Alex Fryer <alex.jeffrey.fryer@gmail.com>
2020-05-28 23:45:56 -0700

    anagrams base case modification

--- recursion/anagrams.js
+++ recursion/anagrams.js
@@ -1,9 +1,6 @@
 const anagrams = (word) => {
   // base cases
   if (word.length === 1) return [word];
-  if (word.length === 2) {
-    return [word, word.split('').reverse().join('')];
-  }
   // recursive cases
   const results = [];
   for (let i = 0; i < word.length; i++) {
